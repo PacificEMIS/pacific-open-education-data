235f32e6fb7f633db901f05ab1e5ef5a55e39a12
Alexander Kirsanov <alex.kirsanov@omega-r.com>
2019-06-26 16:52:57 +0300

    #STORY POED-21_Data_caching

--- pacific_dashboards/lib/src/resources/charts_api_provider.dart
+++ pacific_dashboards/lib/src/resources/charts_api_provider.dart
@@ -4,22 +4,14 @@ import 'dart:convert';
 import 'package:http/http.dart' show Client;
 
 import '../models/teachers_model.dart';
+import 'data_save_file.dart';
 
 class ChartsApiProvider {
   Client client = Client();
 
   final _dataApiKey = 'teachercount';
-  final _baseUrl = "https://fedemis.doe.fm";
 
   Future<TeachersModel> fetchTeachersList() async {
-    final webResponse =
-        await client.get("$_baseUrl/api/warehouse/$_dataApiKey");
-    print(webResponse.body.toString());
-
-    if (webResponse.statusCode == 200) {
-      return TeachersModel.fromJson(json.decode(webResponse.body));
-    } else {
-      throw Exception('Failed to load web data');
-    }
+      return TeachersModel.fromJson(json.decode(await DataSaveFile.loadFileData(_dataApiKey)));
   }
 }
