7a5ffdfc32bdd9ae5be35cc1be3c241a9e7c8196
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-07-11 17:02:33 +0300

    added AppCenter related files

--- pacific_dashboards/lib/main.dart
+++ pacific_dashboards/lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 
 import 'src/App.dart';
 import 'src/ui/InjectorWidget.dart';
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
 
 void main() async {
+  var app_secret = Platform.isAndroid
+      ? "3c6d3883-667f-434e-861a-1e168197aefb"
+      : "a1e06c9c-d224-4549-9e3c-26564d781a52";
+  await AppCenter.start(app_secret,
+      [AppCenterAnalytics.id, AppCenterCrashes.id]);
   var injector = InjectorWidget(child : App());
   await injector.init();
   runApp(injector);
