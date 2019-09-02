7dd3b031c74283c0f9e425a9abb8f54993382b82
knyazev_roman <roman.knyazev@omega-r.com>
2019-09-02 12:17:56 +0300

    ARB updated

--- pacific_dashboards/lib/l10n/messages_all.dart
+++ pacific_dashboards/lib/l10n/messages_all.dart
@@ -16,16 +16,24 @@ import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
 import 'messages_messages.dart' as messages_messages;
+import 'messages_de.dart' as messages_de;
+import 'messages_en.dart' as messages_en;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
   'messages': () => new Future.value(null),
+  'de': () => new Future.value(null),
+  'en': () => new Future.value(null),
 };
 
 MessageLookupByLibrary _findExact(String localeName) {
   switch (localeName) {
     case 'messages':
       return messages_messages.messages;
+    case 'de':
+      return messages_de.messages;
+    case 'en':
+      return messages_en.messages;
     default:
       return null;
   }
