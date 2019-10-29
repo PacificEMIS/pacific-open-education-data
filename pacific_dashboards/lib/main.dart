796c803435b07d7e89d6415d30b6f030a20033fc
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-10-29 09:53:45 +0300

    Revert "killed appcenter deps"
    This reverts commit 757b89f9495b55c90d9067fe0dab43412ef6d652.

--- pacific_dashboards/lib/main.dart
+++ pacific_dashboards/lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 
 import 'src/App.dart';
 import 'src/ui/InjectorWidget.dart';
-// import 'package:appcenter/appcenter.dart';
-// import 'package:appcenter_analytics/appcenter_analytics.dart';
-// import 'package:appcenter_crashes/appcenter_crashes.dart';
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
@@ -17,8 +17,8 @@ void main() async {
   var appSecret = Platform.isAndroid
       ? "3c6d3883-667f-434e-861a-1e168197aefb"
       : "a1e06c9c-d224-4549-9e3c-26564d781a52";
-  // await AppCenter.start(appSecret,
-  //     [AppCenterAnalytics.id, AppCenterCrashes.id]);
+  await AppCenter.start(appSecret,
+      [AppCenterAnalytics.id, AppCenterCrashes.id]);
   var injector = InjectorWidget(child : App());
   await injector.init();
   runApp(injector);
