4bcb9b77b8757e72c1c1e1d6c2cc6dbba54459ad
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-10-30 16:54:28 +0300

    added vendor

--- pacific_dashboards/crashlytics_distribute.sh
+++ pacific_dashboards/crashlytics_distribute.sh
@@ -21,7 +21,7 @@ flutter -v build apk --release --target-platform=android-arm
 # flutter -v build ios --release --no-codesign
 
 cd android
-bundle install
+bundle install --path vendor/bundle
 bundle exec fastlane distribute
 cd ../ios
 # sudo bundle exec fastlane distribute
