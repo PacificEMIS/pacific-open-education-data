5fffc0f608727cf4a41a07f6c8d660358ac5c87f
Nikita Ivanov <nikita.ivanov@omega-r.com>
2019-10-30 16:24:58 +0300

    fix

--- pacific_dashboards/crashlytics_distribute.sh
+++ pacific_dashboards/crashlytics_distribute.sh
@@ -9,9 +9,9 @@ flutter doctor
 
 flutter pub get
 
-cd lib/l10n/
+cd lib/l10n
 rm -fv *.dart
-cd ../../..
+cd ../..
 flutter pub run intl_translation:extract_to_arb --output-dir=lib/l10n /Users/mac/pacific-dashboards/pacific_dashboards/lib/src/utils/Localizations.dart 
 flutter pub run intl_translation:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/l10n/intl_messages.arb lib/l10n/intl_de.arb lib/l10n/intl_en.arb lib/src/utils/localizations.dart
 
