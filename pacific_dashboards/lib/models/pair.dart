8a2554f663ce0ffcc4d6b0d064f4178fe21e9dab
Roman Knyazev <roman.knyazev@omega-r.com>
2020-10-16 13:11:22 +0300

    Removed trash files

--- pacific_dashboards/lib/models/pair.dart
+++
@@ -1,6 +0,0 @@
-class Pair<T1, T2> {
-  final T1 v1;
-  final T2 v2;
-
-  Pair(this.v1, this.v2);
-}
--- pacific_dashboards/lib/shared_ui/bar_chart_data.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BarChartData {
-  final String domain;
-  final num measure;
-  final Color color;
-
-  const BarChartData(this.domain, this.measure, this.color);
-}
